2e5e5ac10fff977c5a503ab3ed56b0485d6ec2a0
Júnior Nascimento <junior.nascm@gmail.com>
2023-09-28 14:53:10 -0300

    setting up three.js

---
+++ main.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+const aspectRation = width / height;
+const fieldOfView = 75;
+const near = 0.1;
+const far = 1000;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  fieldOfView,
+  aspectRation,
+  near,
+  far
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
